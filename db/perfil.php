92979fce4f991d83eec645fcd2f04395e9af4406
Lavínia <laligesiane@gmail.com>
2022-02-13 20:21:19 -0300

    perfil.html

--- db/perfil.php
+++ db/perfil.php
@@ -2,16 +2,12 @@
 <?php
 session_start();
 include('conexao.php');
+
 $id = $_GET['user_id'];
 $nome = $_GET['nome'];
 $login = $_GET['login'];
 $senha = md5($_GET['senha']);
-$connect = mysql_connect('localhost', 
-'user_id',
-'user_nome',
-'user_email',
-'user_senha');
-$db = mysql_select_db('pi_db');
+
 $query_select = "SELECT login FROM usuario WHERE id_user = '$id'";
 $select = mysql_query($query_select,$connect);
 $array = mysql_fetch_array($select);
