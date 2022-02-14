ddd6974178a95c08196eb5ecbb4bb00252e9e4b0
Lavínia <laligesiane@gmail.com>
2022-02-13 22:08:40 -0300

    livro.css

--- db/login.php
+++ db/login.php
@@ -1,29 +1,24 @@
 <?php
 session_start();
-include('conexao.php');
+require('conexao.php');
 
-if(empty($_POST['email']) || empty($_POST['senha'])) {
-	echo "<script> alert ('Preencha todos os campos!'); </script>";
-	header ('Location: ../login.html');
-	exit();
+if(ISSET($_POST['email_login'])){
+	$username = $_POST['email_login'];
+	$password = $_POST['senha_login'];
+
+	$query = mysqli_query($conexao, "SELECT * FROM `usuario` WHERE `user_nome` = '$username' AND `user_senha` = '$password';") or die(mysqli_error());
+	$fetch = mysqli_fetch_array($query);
+	$row = mysqli_num_rows($query);
+
+	if($row > 0){
+		$_SESSION['user_id']=$fetch['user_id'];
+		echo "<script>alert('Login Successfully!')</script>";
+		echo "<script>window.location='../menu/menu.html'</script>";
+	}else{
+		echo "<script>alert('Invalid username or password')</script>";
+		echo "<script>window.location='../login.html'</script>";
+	}
+
 }
-
-$email = mysqli_real_escape_string($conexao, $_POST['email_login']);
-$senha = mysqli_real_escape_string($conexao, $_POST['senha_login']);
-
-$query = "select user_email from usuario where user_email = '$email' and user_senha = md5('$senha');";
-
-$result = msqli_query ($conexao, $query);
-
-$row = mysqli_num_rows();
-
-if ($row == 1) {
-	$_SESSION['email'] = $email;
-	header ('Location: ../menu/menu.html');
-	exit();
-}else{
-	header('Location: ../login.html');
-	exit();
-};
 
 ?>
